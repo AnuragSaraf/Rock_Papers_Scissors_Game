97a5110bb800babbe3650384184f574ae8f34e4c
AnuragSaraf <anurag.saraf@indexnine.com>
2024-01-15 02:01:13 +0530

    Commit 6 : Changes in UI and alert() function

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -11,53 +11,71 @@ function competePC(n){
     let rn = Math.floor(Math.random()*3);
     let score_player = (Number)(score1.innerHTML);
     let score_pc = (Number)(score2.innerHTML);
-    if(score_player>=5 || score_pc>=5){
-        alert((score_player>score_pc?"Player 1 wins. ":"PC wins. ")+'Game Over!')
-        location.reload();
-
-        // return;
+    
+    if(n==0){
+        rock.style.border = "2px solid black";
+        paper.style.border = "1px solid grey";
+        scissor.style.border = "1px solid grey";
     }
+    if(n==1){
+        rock.style.border = "1px solid grey";
+        paper.style.border = "2px solid black";
+        scissor.style.border = "1px solid grey";
+    }
+    if(n==2){
+        rock.style.border = "1px solid grey";
+        paper.style.border = "1px solid grey";
+        scissor.style.border = "2px solid black";
+    }
+
+    
     switch(rn){
         case 0:
             pc.src = 'assets/rock.png';
             console.log("PC selects rock");
             if(n==0){
-                message.innerHTML = 'Its a tie.';
+                message.textContent = 'Its a tie.';
             }
             if(n==1){
-                message.innerHTML = 'You get a point.';
-                score1.innerHTML = score_player+1;
+                message.textContent = 'You get a point.';
+                score1.textContent = score_player+1;
+                score_player++;
             }
             if(n==2){
-                message.innerHTML = 'PC gets a point.';
-                score2.innerHTML = score_pc+1;
+                message.textContent = 'PC gets a point.';
+                score1.textContent = score_pc+1;
+                score_pc++;
             }
             break;
         case 1:
             pc.src = 'assets/paper.png';
             console.log("PC selects paper");
             if(n==0){
-                message.innerHTML = 'PC gets a point.';
-                score2.innerHTML = score_pc+1;
+                message.textContent = 'PC gets a point.';
+                score2.textContent = score_pc+1;
+                score_pc++;
             }
             if(n==1){
-                message.innerHTML = 'Its a tie.';
+                message.textContent = 'Its a tie.';
             }
             if(n==2){
-                message.innerHTML = 'You get a point.';
-                score1.innerHTML = score_player+1;
+                message.textContent = 'You get a point.';
+                score1.textContent = score_player+1;
+                score_player++;
             }
             break;
         case 2:
             pc.src = 'assets/scissors.png';
             console.log("PC selects scissor");
             if(n==0){
-                message.innerHTML = 'You get a point.';
-                score1.innerHTML = score_player+1;
+                message.textContent = 'You get a point.';
+                score1.textContent = score_player+1;
+                score_player++;
             }
             if(n==1){
-                message.innerHTML = 'PC gets a point.';
-                score2.innerHTML = score_pc+1;
+                message.textContent = 'PC gets a point.';
+                score2.textContent = score_pc+1;
+                score_pc++;
             }
             if(n==2){
                 message.innerHTML = 'Its a tie.';
@@ -66,6 +84,12 @@ function competePC(n){
         default:
             console.log("Invalid Selection.",rn);
             break;
+    }
+    
+    if(score_player>=5 || score_pc>=5){
+        setTimeout(() => alert((score_player>score_pc?"Player 1 wins. ":"PC wins. ")+'Game Over!'),1);
+        location.reload();
+        // return;
     }
 }
 
